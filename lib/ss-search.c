fac6f40a091ff4e1eea5d5f396eff1fbe9919d65
Vittorio Parrella <vittorioparrella+1@gmail.com>
2024-05-20 22:45:22 +0200

    fixed issue with manual expanded macro

--- lib/ss-search.c
+++ lib/ss-search.c
@@ -55,7 +55,7 @@ void ss_search_reset(quicly_cc_t *cc, const quicly_loss_t *loss, uint32_t bytes,
 
 	// bin time is the size of each of the sent/delv bins
   uint32_t tmp_bin_time = (loss->rtt.latest * QUICLY_SEARCH_WINDOW_MULTIPLIER) / (QUICLY_SEARCH_DELV_BIN_COUNT);
-  *bin_time = tmp_bin_time < 1 ? tmp_bin_time : 1;
+  *bin_time = tmp_bin_time < 1 ? 1 : tmp_bin_time;
 	*bin_end = now + *bin_time;
 	delv[0] = 0;
 	*bin_rounds = 0;
